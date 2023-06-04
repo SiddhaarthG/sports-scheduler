00419cad199efba9c1ffb850ba1ec8928b61908f
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-06-04 22:46:08 +0530

    eslint formatting

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,5 +1,26 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+const request = require("supertest");
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
 describe("First Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(4000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
   test("First Case", () => {
     expect(true).toBe(true);
   });
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Start of Sports Scheduler')
+const app = require("./app");
+app.listen(3000, () => {
+  console.log("Started express server at port 3000");
+});

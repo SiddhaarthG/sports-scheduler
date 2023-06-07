3bf7054f76b696f1434e4701da7a2179f26d4d11
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-06-07 20:25:36 +0530

    passport setup

--- app.js
+++ app.js
@@ -1,13 +1,88 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 const { request, response } = require("express");
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.set("view engine", "ejs");
+const csrf = require("tiny-csrf");
 
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const LocalStrategy = require("passport-local");
+const flash = require("connect-flash");
+
+const app = express();
+const { Sport, Session, User } = require("./models");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.use(flash());
+
+app.use(
+  session({
+    secret: "my-super-secret-key-852456963741789123",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 24hrs
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
+  next();
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({ where: { email: username } })
+        .then(async (user) => {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch((error) => {
+          return done(error);
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
 
 app.get("/", async (request, response) => {
   response.render("index");

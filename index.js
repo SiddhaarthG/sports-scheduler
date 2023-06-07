8d39c4d01f63e6750323971fe18a284f85a4a9a5
Siddhaarth <gangarajsiddhaarth@gmail.com>
2023-06-07 21:37:33 +0530

    localhost 3000 to 5000

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
 const app = require("./app");
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+app.listen(5000, () => {
+  console.log("Started express server at port 5000");
 });
